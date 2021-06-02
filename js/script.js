2fb1be1a18cb5fe866fe7947ba3e515e29a454d1
Oralbek Tleubaev <keblaro1@gmail.com>
2021-06-02 20:33:53 +0600

    объекты

--- js/script.js
+++ js/script.js
@@ -172,3 +172,64 @@
 // console.log(parseFloat(test));
 
 
+
+// function first() {
+//     // Do something
+//     setTimeout(function() {
+//         console.log(1);
+//     }, 500);
+// }
+// function second() {
+//     console.log(2);
+// }
+// first();
+// second();
+
+// // callback function
+// function learnJS(lang, callback) {
+//     console.log(`Я учу: ${lang}`);
+//     callback();
+// }
+// function done() {
+//     console.log("Я прошел первый урок");
+// }
+// learnJS("JavaScript", done);
+
+
+
+const options = {
+    name: "oralbek",
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: "black",
+        bg: "red"
+    },
+    makeTest: function() {
+        console.log("Test");
+    }
+};
+// console.log(options.name);
+// console.log(options['colors']['border']);
+options.makeTest();
+
+const {border, bg} = options.colors;
+console.log(border);
+
+// delete options.name;
+// console.log(options);
+
+let counter = 0;
+for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+            counter++;
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+        counter++;
+    }
+}
+console.log(counter);                      // подсчитать количество объектов
+console.log(Object.keys(options).length);  // количество объектов, правильный способ
